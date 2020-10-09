c06d8f52d92347e94796dbc253081e3ee2669181
baripembo <erikawei@gmail.com>
2020-10-08 20:11:35 -0400

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -140,47 +140,23 @@ function setupStickyfill() {
 // }
 
 function initSlideshow() {
-  var slideshowController = new ScrollMagic.Controller();
-  
-  // var id = $(this).attr('id');
-  // var containerHeight = $(this).find(' div').innerHeight();
-  // var annotationHeight = $(this).find('.annotation').innerHeight();
-  // var pinSceneTimeline = new TimelineMax();
-  // var newY = containerHeight/2 + annotationHeight/2;
-  // pinSceneTimeline.fromTo($(this).find('.annotation'), 0.2, {y: '+='+viewportHeight/2}, {y: -newY, autoAlpha: 1, ease:Power1.easeNone});
-  var numSlides = $('.slideshow-inner').find('.img-container').length;
-  var pinScene = new ScrollMagic.Scene({
-    triggerElement: '#slideshow', 
-    triggerHook: 0.5,
-    duration: '300%', 
-    offset: viewportHeight/2
-  })
-  //.addIndicators({name: '1'})
-  .setPin('#slideshow')
-  //.setTween(pinSceneTimeline)
-  // .on('update', function(e) {
-  //   console.log(e.target.controller().info('scrollDirection'));
-  // })
-  .on('progress', function (e) {
-    var progress = parseFloat(e.progress.toFixed(2));
-    if (e.target.controller().info('scrollDirection')=='FORWARD') {
-      if (progress>=0.3 && progress<=0.6) {
-        $('.slideshow-inner').find('.img-container[data-slide="3"').css('opacity', 0);
-      }
-      else if (progress>=0.6 && progress<=1) {
-        $('.slideshow-inner').find('.img-container[data-slide="2"').css('opacity', 0);
-      }
-    }
-    else {
-      if (progress<0.6 && progress>=0.3) {
-        $('.slideshow-inner').find('.img-container[data-slide="2"').css('opacity', 1);
-      }
-      else if (progress<0.3 && progress>=0) {
-        $('.slideshow-inner').find('.img-container[data-slide="3"').css('opacity', 1);
-      }
-    }
-  })
-  .addTo(slideshowController);
+  var controller = new ScrollMagic.Controller();
+  var captions = document.querySelectorAll('.caption');
+  for (var i=0; i<captions.length; i++) {
+    new ScrollMagic.Scene({
+        triggerElement: captions[i],
+        triggerHook: 0.9
+      })
+      .on('enter', function(e) {
+        var id = Number($(e.target.triggerElement()).data('caption'));
+        $('img[data-img="'+(id-1)+'"]').css('opacity', 0);
+      })
+      .on('leave', function(e) {
+        var id = Number($(e.target.triggerElement()).data('caption'));
+        $('img[data-img="'+(id-1)+'"]').css('opacity', 1);
+      })
+      .addTo(controller);
+  }
 }
 var map, scroller, main, scrolly, figure, article, step, geoDataArray, viewportHeight;
 var currentIndex = 1;
@@ -202,7 +178,6 @@ $( document ).ready(function() {
   mapboxgl.accessToken = 'pk.eyJ1IjoiaHVtZGF0YSIsImEiOiJja2FvMW1wbDIwMzE2MnFwMW9teHQxOXhpIn0.Uri8IURftz3Jv5It51ISAA';
   viewportHeight = window.innerHeight;
   
-
   function getData() {
     dataUrls.forEach(function (url, index) {
       loadData(url, function (responseText) {
@@ -297,7 +272,8 @@ $( document ).ready(function() {
       console.log('Map loaded')
       getData();
       initJourney();
-      initSections();
+      initPins();
+      //initSections();
       initSlideshow();
     });
   }
@@ -327,32 +303,27 @@ $( document ).ready(function() {
     window.addEventListener('resize', handleResize);
   }
 
-
-  function initSections() {
-    var controller = new ScrollMagic.Controller();
-    
+  function initPins() {
     $('.pin-container').each(function() {
-      var id = $(this).attr('id');
-      var containerHeight = $(this).find('div').innerHeight();
-      var annotationHeight = $(this).find('.annotation').innerHeight();
-      var pinSceneTimeline = new TimelineMax();
-      var newY = containerHeight/2 + annotationHeight/2;
-      pinSceneTimeline.fromTo($(this).find('.annotation'), 0.2, {y: '+='+viewportHeight/2}, {y: -newY, autoAlpha: 1, ease:Power1.easeNone});
+      var item = $(this).find('.pin-item')[0];
+      var pos = Math.round(viewportHeight/2 - $(item).height()/2);
+      $(item).css('top', pos);
+      //$(this).find('.annotation').each(function() {
+        //$(this).find('> div').css('bottom', $(item).height()/2);
+      //});
+    });
 
-      var pinScene = new ScrollMagic.Scene({
-        triggerElement: ('#' + id), 
-        triggerHook: 0.5,
-        duration: '100%', 
-        offset: containerHeight/2,
-      })
-      //.addIndicators({name: '1'})
-      .setPin('#' + id)
-      .setTween(pinSceneTimeline)
-      .addTo(controller);
-    });    
+    //img switch for food security graphic
+    var controller = new ScrollMagic.Controller();
+    var pinScene = new ScrollMagic.Scene({
+      triggerElement: "#slide2",
+      triggerHook: 0.6
+    })
+    .setClassToggle("#pinFoodInsecurity", "showSlide2")
+    .addTo(controller);
   }
 
-  // scrollama event handlers
+
   function handleStepEnter(response) {
     //response = { element, direction, index }
     currentIndex = response.index;
